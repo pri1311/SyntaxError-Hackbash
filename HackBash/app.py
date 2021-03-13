8ffcefe328620fdcf32d8fe5963d940d334504a0
jainam <j.dharod1238@gmail.com>
2021-03-14 00:03:14 +0530

    Update app.py

--- HackBash/app.py
+++ HackBash/app.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 from skimage.transform import resize
 import numpy as np
+from news import news
+from recyclingCentres import query4
 
 app = Flask(__name__)
 
@@ -17,7 +19,7 @@ global sess
 sess = tf.compat.v1.Session()
 tf.compat.v1.keras.backend.set_session(sess)
 global model
-model = load_model(r'D:\Coding\HackBash\GarbageClassifier_Xce.h5')
+model = load_model(r'GarbageClassifier_Xce.h5')
 global graph
 graph = tf.compat.v1.get_default_graph()
 
@@ -27,12 +29,12 @@ def prediction():
 
     file = request.files['file']
     print(file)
-    file.save(r'D:\Coding\HackBash\test.jpg')
+    file.save(r'test.jpg')
     # Step 1
     # my_image = plt.imread(os.path.join('/content/drive/My Drive/flask/uploads', filename))
 
     # Step 2
-    my_image = plt.imread(r'D:\Coding\HackBash\test.jpg')
+    my_image = plt.imread(r'test.jpg')
     my_image_re = resize(my_image, (150, 150))
 
     x = np.expand_dims(my_image_re, axis=0)
@@ -56,7 +58,39 @@ def prediction():
         "prob1": save[0]["prob"],
     }
     msg = {'class': class1}
-    os.remove(r'D:\Coding\HackBash\test.jpg')
+    os.remove(r'test.jpg')
+    return jsonify(msg)
+
+
+data1 = {}
+
+
+@app.route('/news', methods=['POST'])
+def scrape():
+    global data1
+    msg = ""
+    html = news.make_request()
+    data = news.parse_content(html)
+    if data:
+        data1 = {'data': data}
+        msg = {"status": {"type": "success", "data": 0, "message": data}}
+    else:
+        msg = {"status": {"type": "failure", "message": "Missing Data"}}
+    return jsonify(msg)
+
+
+@app.route('/recyclingCentres', methods=['POST'])
+def web():
+    global data1
+    msg = ""
+
+    html = query4.make_request()
+    data = query4.parse_content(html)
+    if data:
+        data1 = {'data': data}
+        msg = {"status": {"type": "success", "data": 4, "message": data}}
+    else:
+        msg = {"status": {"type": "failure", "message": "Missing Data"}}
     return jsonify(msg)
 
 
